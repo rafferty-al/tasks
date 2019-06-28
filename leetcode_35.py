6a0b44b84d7f1c594149d2ae788e4a6eb80368b6
rafferty-al <sashasamsungurzhum@mail.ru>
2019-06-28 16:39:56 +0300

    35 rewritten

--- leetcode_35.py
+++ leetcode_35.py
@@ -1,5 +1,5 @@
 # https://leetcode.com/problems/search-insert-position/
-# Runtime: 36 ms, faster than 79.84% of Python3 online submissions for Search Insert Position.
+# Runtime: 40 ms, faster than 31.78% of Python3 online submissions for Search Insert Position.
 
 def searchInsert(nums, target):
     index = 0
@@ -8,19 +8,10 @@ def searchInsert(nums, target):
             index = num
             break
         else:
-            index = None
-    if index is None:
-        for num in range(len(nums)):
-            if target == 0:
-                index = 0
-                break
-            elif nums[num] >= target:
-                index = num
-                break
-            else:
-                index = len(nums)
+            if nums[num] <= target:
+                index = num + 1
     return index
 
 
 if __name__ == '__main__':
-    print(searchInsert([1,3,5,6], 2))
+    print(searchInsert([1,3,5,6], 0))
